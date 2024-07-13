c8ff700d0c5d67929857292e26c016ed625ea2f1
Victor Felippe <vitor.lippe.dias@gmail.com>
2024-07-13 05:02:32 -0300

    implementando coleção de dados

--- src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -7,6 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosTemporada(@JsonAlias("Season") Integer numero,
-                             @JsonAlias("Episodes") List<DadosEpisodio> episodio) {
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
     
 }
